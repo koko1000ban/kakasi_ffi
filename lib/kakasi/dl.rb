94468c9ddda55e57d9675bc270e3d4c7033f682b
tabito ohtani <koko1000ban@gmail.com>
2015-10-08 12:23:32 +0900

    Suppress wanring when require 'dl/import'

--- lib/kakasi/dl.rb
+++ lib/kakasi/dl.rb
@@ -1,6 +1,17 @@
+def suppress_warning
+  require 'stringio'
+  old_stderr = $stderr
+  $stderr = StringIO.new
+  yield
+ensure
+  $stderr = old_stderr
+end
+
 module Kakasi
   module Lib
-    require 'dl/import'
+    suppress_warning do
+      require 'dl/import'
+    end
     Fiddle = DL
     Pointer = DL::CPtr
   end
